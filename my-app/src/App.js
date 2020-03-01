12764597b621e4312dbce1f0a0a72a2ec2d26189
Josue Rodriguez <jr2589213@gmail.com>
2020-02-29 22:17:01 -0800

    Setup redux skeleton on application
    *Created basic reducer
    
    *Added utils and actions folder
    
    *Setup thunk/browserrouter/and provider in index.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Container} from './components/Container'
 
 function App() {
   return (
     <div className="App">
       {/* API: https://jobs.github.com/positions.json?search=react */}
+      <Container/>
     </div>
   );
 }
