0d8e9c01ffd777a6fdf4c336c68bc925a9958aab
Josue Rodriguez <jr2589213@gmail.com>
2020-02-29 22:05:11 -0800

    Initialize Repo - initial commit

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      {/* API: https://jobs.github.com/positions.json?search=react */}
+    </div>
+  );
+}
+
+export default App;
